d752d044abcc7ed5a58f80070cb654657bfc1ede
Ana Michelle Lopez Hernandez <ing.amichellelopezhdz@gmail.com>
2025-09-10 12:25:16 -0600

    Task board, linear gradient

--- app/index.tsx
+++ app/index.tsx
@@ -1,14 +1,11 @@
-import TaskList from "@/src/components/TaskList";
-import { SafeAreaView, StatusBar, StyleSheet, View } from "react-native";
+import TaskBoard from "@/src/components/TaskBoard";
+import { StyleSheet, View } from "react-native";
 
 export default function Page() {
   return (
-    <SafeAreaView style={styles.container}>      
-      <View style={{padding:10}}>
-        <TaskList/>        
-      </View>      
-      <StatusBar />
-    </SafeAreaView>
+    <View style={styles.container}>      
+      <TaskBoard/>    
+    </View>
   );
 }
 
@@ -16,19 +13,5 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-  },
-  main: {
-    flex: 1,
-    justifyContent: "center",
-    maxWidth: 960,
-    marginHorizontal: "auto",
-  },
-  title: {
-    fontSize: 64,
-    fontWeight: "bold",
-  },
-  subtitle: {
-    fontSize: 36,
-    color: "#38434D",
   },
 });
---
+++ src/components/TaskBoard.tsx
@@ -0,0 +1,13 @@
+import { LinearGradient } from 'expo-linear-gradient';
+import { StyleSheet, View } from "react-native";
+import TaskList from "./TaskList";
+
+export default function TaskBoard () {
+    return (
+        <View style={{padding:10, flex:1}}>
+            <LinearGradient colors={['#8711c1', '#2472fc']}
+                style={StyleSheet.absoluteFill} />
+            <TaskList/>        
+        </View>  
+    )
+}
