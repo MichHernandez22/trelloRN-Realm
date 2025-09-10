9f9885637905806f20019d984848349c54669273
Ana Michelle Lopez Hernandez <ing.amichellelopezhdz@gmail.com>
2025-09-10 12:05:57 -0600

    New Task input

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -1,15 +1,24 @@
 import { useState } from "react";
-import { FlatList, Text, View } from "react-native";
+import { Button, FlatList, StyleSheet, Text, TextInput, View } from "react-native";
 import TaskListItem from "./TaskListItem";
 
 export default function TaskList(){
     const [tasks, setTasks] = useState([
         { description: 'First tasks'},
         { description: 'Second tasks'}
-    ])
+    ]);
+
+    const [newTask, setNewTask] = useState('');
+
+    const createTask = () =>{
+        console.warn('Create: ', newTask)
+        setTasks([...tasks, {description: newTask}])
+        setNewTask('')
+    }
+
     return(
-        <View style={{backgroundColor: '#101112', padding: 10, borderRadius: 5}}>
-            <Text style={{color: 'white', fontWeight: 'bold', fontSize: 20, marginVertical:10 }}>TODO</Text>
+        <View style={styles.container}>
+            <Text style={styles.title}>TODO</Text>
             <FlatList 
                 data={tasks} 
                 renderItem={({item})=> <TaskListItem task={item}/>}
@@ -18,6 +27,20 @@ export default function TaskList(){
             
             {/*<TaskListItem task={{description:'Second Task'}}/>
             <TaskListItem task={{description:'Third Task'}}/>*/}
+
+            <TextInput 
+                value={newTask}
+                onChangeText={setNewTask}
+                placeholder="New Task" 
+                placeholderTextColor="gray" 
+                style={styles.input} />
+            <Button title="Add task" onPress={createTask} />
         </View>
     )
 }
+
+const styles = StyleSheet.create({
+    container: {backgroundColor: '#101112', padding: 10, borderRadius: 5, gap:10},
+    title: {color: 'white', fontWeight: 'bold', fontSize: 20, marginVertical:10 },
+    input: {color: 'white', padding: 15, backgroundColor: '#1D2125', borderRadius: 5 }
+})
