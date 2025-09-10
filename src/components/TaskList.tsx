0c10f9432078ef4e30a06319ee3fc0256b09745d
Ana Michelle Lopez Hernandez <ing.amichellelopezhdz@gmail.com>
2025-09-10 11:30:04 -0600

    Task List

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -1,13 +1,23 @@
-import { Text, View } from "react-native";
+import { useState } from "react";
+import { FlatList, Text, View } from "react-native";
 import TaskListItem from "./TaskListItem";
 
 export default function TaskList(){
+    const [tasks, setTasks] = useState([
+        { description: 'First tasks'},
+        { description: 'Second tasks'}
+    ])
     return(
-        <View style={{backgroundColor: '#101112'}}>
-            <Text>TODO</Text>
-            <TaskListItem task={{description:'First Task'}}/>
-            <TaskListItem task={{description:'Second Task'}}/>
-            <TaskListItem task={{description:'Third Task'}}/>
+        <View style={{backgroundColor: '#101112', padding: 10, borderRadius: 5}}>
+            <Text style={{color: 'white', fontWeight: 'bold', fontSize: 20, marginVertical:10 }}>TODO</Text>
+            <FlatList 
+                data={tasks} 
+                renderItem={({item})=> <TaskListItem task={item}/>}
+                contentContainerStyle={{ gap: 5}}
+            />
+            
+            {/*<TaskListItem task={{description:'Second Task'}}/>
+            <TaskListItem task={{description:'Third Task'}}/>*/}
         </View>
     )
 }
